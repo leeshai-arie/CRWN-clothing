7b1d5fd84654c4077227137ba1b351f6eecbcf7a
Leeshai Arie <leeshai.arie@imubit.com>
2022-09-07 11:25:30 +0300

    changed app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  return(
+    <div className="categories-container">
+      <div className="category-contaier">
+      <div className="category-body-container">
+        <h2>Hats</h2>
+        <p>Shop Now</p>
+      </div>
+      </div>
     </div>
   );
 }
