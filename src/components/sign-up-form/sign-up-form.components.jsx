07833b21d2087779bda7b870835559121dab285f
leeshai-arie <leeshaiarie@gmail.com>
2022-10-26 14:53:08 +0300

    updated listner for the login

--- src/components/sign-up-form/sign-up-form.components.jsx
+++ src/components/sign-up-form/sign-up-form.components.jsx
@@ -18,6 +18,7 @@ const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
 
+
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
   };
@@ -32,7 +33,8 @@ const SignUpForm = () => {
 
     try {
       const { user } = await createAuthUserWithEmailAndPassword(email, password);
-      await createUserDocumentFromAuth(user, { displayName });
+      await createUserDocumentFromAuth(user, { displayName }); 
+      
       resetFormFields();
     } catch(error) {
       if(error.code === "auth/email-already-in-use") {
