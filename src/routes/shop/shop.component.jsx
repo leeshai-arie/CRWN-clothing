84b9d2d8e67bc5657cd24c10be0f3316950b60d4
leeshai-arie <leeshaiarie@gmail.com>
2022-11-06 19:00:24 +0200

    added pr oduct-card component

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,6 +1,17 @@
+import { useContext } from "react";
+
+import { ProductsContext } from "../../contexts/products.context";
+
 const Shop = () => {
+  const {products} = useContext(ProductsContext);
   return(
-    <div>Shop</div>
+    <div>
+      {products.map(({id, name}) => (
+        <div key={id}>
+          <h1>{name}</h1>
+        </div>
+      ))}
+    </div>
   );
 };
 
