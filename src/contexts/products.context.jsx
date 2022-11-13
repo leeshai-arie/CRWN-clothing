27c5f791f8d674eaaa954d223cf9330b30a62b01
Leeshai Arie <leeshai.arie@imubit.com>
2022-11-13 21:11:06 +0200

    updated cartdropdown

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -9,7 +9,5 @@ export const ProductsContext = createContext({
 export const ProductProvider = ({children}) => {
   const [products, setProducts] = useState(PRODUCTS);
   const value = {products};
-  return(
-    <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>
-  );
+  return(<ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>);
 };
