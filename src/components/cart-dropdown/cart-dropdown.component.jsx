f59a4f9fac559c3573f6e28c55d9960827033d41
Leeshai Arie <leeshai.arie@imubit.com>
2022-12-27 18:09:46 +0200

    updaated checkoutpage

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -10,13 +10,18 @@ import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const goToCheckoutHandler = () => {
+    navigate("/checkout")
+  }
 
   return(
     <div className="cart-dropdown-container">
       <div className="cart-items">
         {cartItems.map((item) => (<CartItem key={item.id} cartItem={item} />))}
       </div>
-      <Button>Go To Checkout</Button>
+      <Button onClick={goToCheckoutHandler}>Go To Checkout</Button>
     </div>
   );
 };
